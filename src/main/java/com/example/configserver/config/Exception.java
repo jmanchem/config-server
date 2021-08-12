9c95a881fc69057897fe5a6f8d47dd9fe09309ad
jmanchem <88394788+jmanchem@users.noreply.github.com>
2021-08-12 16:10:35 -0500

    config-server

---
+++ src/main/java/com/example/configserver/config/Exception.java
@@ -0,0 +1,13 @@
+package com.example.configserver.config;
+
+public class Exception {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
